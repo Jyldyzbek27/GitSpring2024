3bd57c4f574e3c3e8666b2eeb03ae2aed39bd380
Jyldyzbek Kubatbekov <KubatbekovVv@gmail.com>
2024-08-12 15:01:34 +0600

    String1/MakeOutWord

---
+++ src/main/java/codingbat/string1/MakeOutWord.java
@@ -0,0 +1,10 @@
+package codingbat.string1;
+
+public class MakeOutWord {
+    public String makeOutWord(String out, String word) {
+        String str = out.substring(0, 2);
+        String str1 = out.substring(2);
+        return str + word + str1;
+    }
+
+}
